c6068ac6616862950c95ea1079a54dcb48b4236b
Sandra Jimenez <lacakecontact@gmail.com>
2023-01-17 15:58:52 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import App from './App';
 import { HashRouter } from 'react-router-dom';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( 
+ReactDOM.render(
     <HashRouter>
     <App/>
 </HashRouter>
