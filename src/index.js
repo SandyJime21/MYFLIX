f5dd1d80e0c88763605270d80580278582c956b2
Sandra Jimenez <lacakecontact@gmail.com>
2023-01-17 12:46:12 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <Router >
-    <App/>
+root.render( 
+<Router basename = {'/MYFLIX' }>
+    <App />
     </Router>
 
 );
